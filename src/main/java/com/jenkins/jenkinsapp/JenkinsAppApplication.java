fbcecdba8a1286159985e9adb8e81a04441f604e
chaimae <chaimaer759@gmail.com>
2024-11-04 22:36:55 +0100

    Add Jenkinsfile

--- src/main/java/com/jenkins/jenkinsapp/JenkinsAppApplication.java
+++ src/main/java/com/jenkins/jenkinsapp/JenkinsAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsAppApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello jenkins");
         SpringApplication.run(JenkinsAppApplication.class, args);
     }
 
